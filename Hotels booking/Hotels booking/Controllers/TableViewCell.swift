64f21638f3a11c85a5b1dccddd9ab9b09c75a239
viktor <fpmi.graskovviktor@gmail.com>
2020-05-14 19:57:01 +0300

    add main view and transition between first views and main view

---
+++ Hotels booking/Hotels booking/Controllers/TableViewCell.swift
@@ -0,0 +1,24 @@
+//
+//  TableViewCell.swift
+//  Hotels booking
+//
+//  Created by Viktor on 5/14/20.
+//  Copyright © 2020 Viktor. All rights reserved.
+//
+
+import UIKit
+
+class TableViewCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
