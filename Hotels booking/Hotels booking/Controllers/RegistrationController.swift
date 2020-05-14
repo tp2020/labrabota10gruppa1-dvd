12c08ab70b0db751852b5bd991e9c7f7b96075f3
dmitars <Dmitriy.tarasienko@gmail.com>
2020-05-14 11:25:18 -0700

    feat: add database

--- Hotels booking/Hotels booking/Controllers/RegistrationController.swift
+++ Hotels booking/Hotels booking/Controllers/RegistrationController.swift
@@ -7,6 +7,7 @@
 //
 
 import UIKit
+import CoreData
 
 class RegistrationController: UIViewController {
 
@@ -42,4 +43,94 @@ class RegistrationController: UIViewController {
         }
     }
 
+    
+    func addHotel(name:String,descriptions:String,country:String,city:String,numbOfLuxRooms:Int,numbOfStRooms:Int,luxPrice:Int,
+                  standardPrice:Int){
+          guard let appDelegate =
+                UIApplication.shared.delegate as? AppDelegate else {
+                return
+              }
+        let managedContext =
+          appDelegate.persistentContainer.viewContext
+        
+        let entity =
+                NSEntityDescription.entity(forEntityName: "Hotel",
+                                           in: managedContext)!
+              let hotel = NSManagedObject(entity: entity,
+                                            insertInto: managedContext)
+              hotel.setValue(city, forKeyPath: "city")
+              hotel.setValue(name, forKey: "name")
+              hotel.setValue(descriptions,forKey: "descriptions")
+        hotel.setValue(numbOfLuxRooms,forKey: "numbOfLuxRooms")
+        hotel.setValue(numbOfStRooms, forKey: "numbOfStRooms")
+        hotel.setValue(luxPrice, forKey: "luxPrice")
+        hotel.setValue(standardPrice, forKey: "standardPrice")
+        do {
+          try managedContext.save()
+        } catch let error as NSError {
+          print("Could not save. \(error), \(error.userInfo)")
+        }
+    }
+    
+    func addRoute(transportType:String,cityFrom:String,cityTo:String,price:Int,time:String,timeOfArrive:String,numbOfTickets:Int){
+        guard let appDelegate =
+                UIApplication.shared.delegate as? AppDelegate else {
+                return
+              }
+        let managedContext =
+          appDelegate.persistentContainer.viewContext
+        
+        let entity =
+                NSEntityDescription.entity(forEntityName: "Route",
+                                           in: managedContext)!
+              let route = NSManagedObject(entity: entity,
+                                            insertInto: managedContext)
+        route.setValue(cityFrom,forKey: "cityFrom")
+        route.setValue(cityTo,forKey: "cityTo")
+        route.setValue(time,forKey: "time")
+        route.setValue(timeOfArrive,forKey: "timeOfArrive")
+        route.setValue(price,forKey: "Price")
+        route.setValue(numbOfTickets,forKey: "numbOfTickets")
+        let entityTransport =
+        NSEntityDescription.entity(forEntityName: "Transport",
+                                   in: managedContext)!
+        let transport = NSManagedObject(entity: entityTransport,
+                                      insertInto: managedContext)
+        transport.setValue(transportType, forKey: "type")
+        route.mutableSetValue(forKey: "transport").add(transport)
+        do {
+          try managedContext.save()
+        } catch let error as NSError {
+          print("Could not save. \(error), \(error.userInfo)")
+        }
+    }
+    
+    func addReservation(hotel:NSManagedObject,route:NSManagedObject,typeOfRoom:String){
+        guard let appDelegate =
+                UIApplication.shared.delegate as? AppDelegate else {
+                return
+              }
+        let managedContext =
+          appDelegate.persistentContainer.viewContext
+        
+        let entity =
+                NSEntityDescription.entity(forEntityName: "Reservation",
+                                           in: managedContext)!
+        let reservation = NSManagedObject(entity: entity,
+        insertInto: managedContext)
+        
+        reservation.setValue(typeOfRoom, forKey: "typeOfRoom")
+        reservation.mutableSetValue(forKey: "hotel").add(hotel)
+        reservation.mutableSetValue(forKey: "route").add(route)
+        let date = Date()
+        let formatter = DateFormatter()
+        let dateString = formatter.string(from: date)
+        reservation.setValue(dateString,forKey: "time")
+        
+        do {
+          try managedContext.save()
+        } catch let error as NSError {
+          print("Could not save. \(error), \(error.userInfo)")
+        }
+    }
 }
